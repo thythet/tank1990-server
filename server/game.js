e7622a770fefbdb24a8066cf50b70c7e7b0c7128
thythet <thythet455@gmail.com>
2025-10-17 09:40:11 +0700

    Update game.js

--- server/game.js
+++ server/game.js
@@ -1,2 +1,22 @@
-// placeholder game.js
-console.log("Game loaded");
+// server/game.js
+
+function setupSocket(io) {
+  console.log("Socket setup started");
+
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+    });
+
+    // You can add more game events here
+    socket.on("move", (data) => {
+      console.log("Player move:", data);
+      // broadcast move to other players
+      socket.broadcast.emit("move", data);
+    });
+  });
+}
+
+module.exports = { setupSocket };
